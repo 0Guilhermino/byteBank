83de1beddf087d16b52a995e29066bfa0c8ac386
Angelo Guilhermino <angeloguilhermino@gmail.com>
2023-02-13 21:50:14 -0300

    first commit

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter, Output } from "@angular/core";
+import { Transferencia } from "src/models/transferencia";
+import { TransferenciaService } from "../services/transferencia.service";
+
+@Component({
+    selector: 'app-nova-transferencia',
+    templateUrl: './nova-transferencia.component.html',
+    styleUrls: ['./nova-transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent {
+
+    @Output() aoTransferir = new EventEmitter<any>();
+   
+    valor: number ;
+    destino: number;
+
+    constructor(private service: TransferenciaService){}
+
+    transferir(){
+        console.log("Solicitada nova transferência");
+
+        const valorEmitir: Transferencia = { valor: this.valor, destino: this.destino }
+
+        this.service.adicionarNovaTransferencia(valorEmitir).subscribe(resultado => {
+            console.log(resultado);
+            this.limparFormulario();
+        },
+        error => console.log(error)
+        );
+        
+        
+    }
+
+    limparFormulario(){
+        this.valor = null;
+        this.destino = null;
+    }
+
+}
---
+++ src/app/services/transferencia.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from "@angular/common/http"
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Transferencia } from "src/models/transferencia";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TransferenciaService {
+    private listaDeTransferencia: any[];
+    private URL = "http://localhost:3000/transferencias";
+    
+    constructor(private httpClient: HttpClient) {
+        this.listaDeTransferencia = [];
+    }
+
+    get transferencias(){
+        return this.listaDeTransferencia;
+    }
+
+    ExibeTodasTransferências(): Observable<Transferencia[]>{
+        return this.httpClient.get<Transferencia[]>(this.URL)
+    }
+
+    adicionarNovaTransferencia(transferencia: any): Observable<Transferencia>{
+        this.hidratar(transferencia);
+
+        return this.httpClient.post<Transferencia>(this.URL,transferencia);
+    }
+
+    private hidratar(transferencia: any){
+        transferencia.data = new Date();
+    }
+
+}
